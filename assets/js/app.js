a38f0ffab5e39e96aba92eb9da828d6a7f875285
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-05-23 10:54:33 +0530

    section 5 slick slider arrow update

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,3 +53,65 @@ $('.sec3-slider').slick({
         }
     ]
 });
+
+$('.sec5-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    centerPadding: '5px',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    adaptiveHeight: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 2
+            }
+        }
+    ]
+});
+
+$('.sec6-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    centerPadding: '5px',
+    prevArrow: $('.prev1'),
+    nextArrow: $('.next1'),
+    adaptiveHeight: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 2
+            }
+        }
+    ]
+});
